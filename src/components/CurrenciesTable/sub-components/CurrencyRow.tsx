e473a2fb7a1500d9a7057a422b42853ef2617661
Dmitry Kulak <dmitry.kulak11@gmail.com>
2022-03-15 18:49:05 +0300

    fixed styles, converted diff to percentage

--- src/components/CurrenciesTable/sub-components/CurrencyRow.tsx
+++ src/components/CurrenciesTable/sub-components/CurrencyRow.tsx
@@ -7,15 +7,20 @@ type CurrencyRowProps = Currency;
 
 export const CurrencyRow = React.memo(
   ({ name, code, value, previousValue }: CurrencyRowProps) => {
-    const diff = +(value - previousValue).toFixed(4);
+    const diff = +(((value - previousValue) / value) * 100).toFixed(2);
 
-    const diffStyle = diff > 0 ? styles.diffPositive : styles.diffNegative;
+    const diffColor = diff > 0 ? styles.diffPositive : styles.diffNegative;
+
+    const diffStyle = `${styles.cell} ${styles.diffCell} ${diffColor}`;
 
     return (
       <tr data-tip={name} className={styles.row}>
         <td className={styles.cell}>{code}</td>
         <td className={styles.cell}>{value}</td>
-        <td className={`${styles.cell} ${diffStyle}`}>{diff}</td>
+        <td className={diffStyle}>
+          {diff > 0 && "+"}
+          {diff} %
+        </td>
       </tr>
     );
   }
